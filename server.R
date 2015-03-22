1e8a229beb674fab120907fb5f9dd0cda45d5021
newcombe <caroline.newcombe@gmail.com>
2015-03-22 08:58:06 -0400

    Coursera shiny app

---
+++ server.R
@@ -0,0 +1,9 @@
+wtkilo <- function(wt) wt * 2.2
+
+library(shiny)
+shinyServer(
+  function(input, output) {
+    output$inputValue <- renderPrint({input$wt})
+    output$prediction <- renderPrint({wtkilo(input$wt)})
+  }
+)
---
+++ ui.R
@@ -0,0 +1,18 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Lbs to Kg Conversion Tool"),
+  sidebarPanel(
+    h1('Input'),
+    numericInput("wt", "Enter your weight (in pounds):", 0),
+    submitButton('Submit')
+  ),
+  mainPanel(
+    h3('Output'),
+    h4('You entered'),
+    verbatimTextOutput("inputValue"),
+    h4('Your weight in kilograms is'),
+    verbatimTextOutput("prediction"),
+    p("Documentation:  It's easy to get started!  Just enter your weight, in pounds,
+      in the input box.  Press Submit, and your weight will be displayed in kilograms!")
+  )
+))
