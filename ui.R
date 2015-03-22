06b34061f4418d66d1335a0a9db1d4bd3c14d35d
newcombe <caroline.newcombe@gmail.com>
2015-03-22 09:01:56 -0400

    updated ui,R

--- ui.R
+++ ui.R
@@ -13,6 +13,7 @@ shinyUI(pageWithSidebar(
     h4('Your weight in kilograms is'),
     verbatimTextOutput("prediction"),
     p("Documentation:  It's easy to get started!  Just enter your weight, in pounds,
-      in the input box.  Press Submit, and your weight will be displayed in kilograms!")
+      in the input box.  Press Submit, and your weight will be displayed in kilograms!
+      See this github page for the ui.R and server.R files:  https://github.com/newcombe/shiny")
   )
 ))
